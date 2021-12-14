38cbc4a4fe54643fa1eabe01c9f3dd2bf38d36ff
Tom Cowland <tom@foundry.com>
2021-12-14 15:02:24 +0000

    [Core] Add missing docs to `Host`/`HostInterface`
    Part of #101.
    
    Signed-off-by: Tom Cowland <tom@foundry.com>

--- python/openassetio/hostAPI/HostInterface.py
+++ python/openassetio/hostAPI/HostInterface.py
@@ -13,6 +13,9 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 #
+"""
+A single-class module, providing the HostInterface class.
+"""
 
 __all__ = ['HostInterface']
 
