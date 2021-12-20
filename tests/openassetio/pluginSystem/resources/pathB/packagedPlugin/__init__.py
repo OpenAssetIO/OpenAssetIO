106b2998358cf84711809dd1dba8e2adba13d323
Tom Cowland <tom@foundry.com>
2021-12-20 12:53:09 +0000

    [PluginSystem] Suppress erroneous `import-failed`
    `pylint` thinks this is a failed import, but it's actually fine due
    to the way the plugin system loads code.
    
    Part of #101.
    
    Signed-off-by: Tom Cowland <tom@foundry.com>

--- tests/openassetio/pluginSystem/resources/pathB/packagedPlugin/__init__.py
+++ tests/openassetio/pluginSystem/resources/pathB/packagedPlugin/__init__.py
@@ -2,6 +2,9 @@
 Provides a test PluginSystemPlugin implemented within a package.
 """
 
+# pylint gets upset, but this is fine due to
+# the way the plugin system loads plugins.
+# pylint: disable=import-error
 from .PackagePlugin import PackagePlugin
 
 # pylint: disable=invalid-name
