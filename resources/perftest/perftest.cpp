392bdb5484e075575aaca9665c878afbd6bcb567
Tom Cowland <tom@foundry.com>
2022-05-27 09:09:08 +0100

    [Resources] Fix linter/build warnings in `perftest.cpp`
    These have prevented us enabling it by default in test builds, which
    has on occasions meat we've missed when it has been broken by changes.
    
    This is a quick band-aid that applies the minimal changes, rather than
    any particularly considered fix.
    
    Closes #399.
    
    Signed-off-by: Tom Cowland <tom@foundry.com>

--- resources/perftest/perftest.cpp
+++ resources/perftest/perftest.cpp
@@ -8,12 +8,12 @@
 
 #include <openassetio/TraitsData.hpp>
 #include <openassetio/trait/TraitBase.hpp>
-using namespace openassetio;
+using namespace openassetio; // NOLINT(google-build-using-namespace)
 
-constexpr size_t nIterations = 100000;
-const size_t dbSize = 1000000;
+constexpr size_t kNIterations = 100000;
+const size_t kDbSize = 1000000;
 
-std::chrono::nanoseconds MeasureRuntime(std::function<void(void)> func)
+std::chrono::nanoseconds measureRuntime(const std::function<void(void)>& func)
 {
   const auto start = std::chrono::high_resolution_clock::now();
   func();
@@ -21,9 +21,9 @@ std::chrono::nanoseconds MeasureRuntime(std::function<void(void)> func)
   return stop - start;
 }
 
-auto MeasureRuntimeMs(std::function<void(void)> func)
+auto measureRuntimeMs(const std::function<void(void)>& func)
 {
-  return std::chrono::duration_cast<std::chrono::milliseconds>(MeasureRuntime(func)).count();
+  return std::chrono::duration_cast<std::chrono::milliseconds>(measureRuntime(func)).count();
 }
 
 ///////////////////////////////////////////////////////////
@@ -32,12 +32,12 @@ const std::string kSubDirectory = "0123456789_0123456789_0123456789/";
 
 std::string getEntityReference(size_t i)
 {
-  return std::string("ams://" + kSubDirectory + "myasset_") + std::to_string(i);
+  return "ams://" + kSubDirectory + "myasset_" + std::to_string(i);
 }
 
 std::string getPrimaryString(size_t i)
 {
-  return std::string("file://" + kSubDirectory + "myasset_") + std::to_string(i);
+  return "file://" + kSubDirectory + "myasset_" + std::to_string(i);
 }
 
 ///////////////////////////////////////////////////////////
@@ -61,16 +61,16 @@ struct BlobTrait : trait::TraitBase<BlobTrait> {
 class OldManagerInterface
 {
 public:
-  OldManagerInterface(const std::unordered_map<std::string, std::string>& refToPrimStrs) : refToPrimStrs{refToPrimStrs} {}
+  explicit OldManagerInterface(const std::unordered_map<std::string, std::string>& refToPrimStrs) : refToPrimStrs_{refToPrimStrs} {}
   std::string resolveEntityReference(const std::string& ref);
 
 private:
-  const std::unordered_map<std::string, std::string>& refToPrimStrs;
+  const std::unordered_map<std::string, std::string>& refToPrimStrs_;
 };
 
 std::string OldManagerInterface::resolveEntityReference(const std::string& ref)
 {
-  return refToPrimStrs.at(ref);
+  return refToPrimStrs_.at(ref);
 }
 
 ///////////////////////////////////////////////////////////
@@ -79,24 +79,24 @@ auto benchmarkOldManager(const std::unordered_map<std::string, std::string>& ref
   OldManagerInterface oldMgr(refToPrimStrs);
 
   const auto doLookups = [&oldMgr, &refs]() {
-    for(size_t i = 0; i < nIterations; ++i) {
+    for(size_t i = 0; i < kNIterations; ++i) {
       const auto& ref = refs[i];
-      const auto& primStr = oldMgr.resolveEntityReference(ref);
+      [[maybe_unused]] const auto& _ = oldMgr.resolveEntityReference(ref);
     }
   };
 
-  return MeasureRuntimeMs(doLookups);
+  return measureRuntimeMs(doLookups);
 }
 
 ///////////////////////////////////////////////////////////
 class NewManagerInterface
 {
 public:
-  NewManagerInterface(const std::unordered_map<std::string, std::string>& refToPrimStrs) : refToPrimStrs{refToPrimStrs} {}
+  explicit NewManagerInterface(const std::unordered_map<std::string, std::string>& refToPrimStrs) : refToPrimStrs_{refToPrimStrs} {}
   std::shared_ptr<TraitsData> resolve(const std::string& ref, const TraitsData::TraitSet& traitSet);
 
 private:
-  const std::unordered_map<std::string, std::string>& refToPrimStrs;
+  const std::unordered_map<std::string, std::string>& refToPrimStrs_;
 };
 
 std::shared_ptr<TraitsData> NewManagerInterface::resolve(const std::string& ref, const TraitsData::TraitSet& traitSet)
@@ -110,7 +110,7 @@ std::shared_ptr<TraitsData> NewManagerInterface::resolve(const std::string& ref,
   auto result = std::make_shared<TraitsData>(populatedTraits);
   if(getPath) {
     BlobTrait btrait(result);
-    btrait.setUrl(refToPrimStrs.at(ref));
+    btrait.setUrl(refToPrimStrs_.at(ref));
   }
   return result;
 }
@@ -121,47 +121,49 @@ auto benchmarkNewManager(const std::unordered_map<std::string, std::string>& ref
   NewManagerInterface newMgr(refToPrimStrs);
 
   const auto doLookups = [&newMgr, &refs]() {
-    for(size_t i = 0; i < nIterations; ++i) {
+    for(size_t i = 0; i < kNIterations; ++i) {
       const auto& ref = refs[i];
       const auto spec = newMgr.resolve(ref, {BlobTrait::kId});
       BlobTrait btrait(spec);
       Str url;
-      const auto success = btrait.getUrl(&url);
+      [[maybe_unused]] const auto _ = btrait.getUrl(&url);
     }
   };
 
-  return MeasureRuntimeMs(doLookups);
+  return measureRuntimeMs(doLookups);
 }
 
 ///////////////////////////////////////////////////////////
 int main()
 {
-  std::cout << "CAUTION: on Windows do not run this in a Visual Studio command prompt "
-    << "('x64 Native Tools Command Prompt for VS') or the measurements may fluctuate, "
-    << "use a basic CMD shell" << std::endl;
+  try { // bugprone-exception-escape
+    std::cout << "CAUTION: on Windows do not run this in a Visual Studio command prompt "
+      << "('x64 Native Tools Command Prompt for VS') or the measurements may fluctuate, "
+      << "use a basic CMD shell" << std::endl;
 
-  std::cout << "       Iterations: " << nIterations << std::endl;
-  std::cout << "AMS database size: " << dbSize << std::endl;
+    std::cout << "       Iterations: " << kNIterations << std::endl;
+    std::cout << "AMS database size: " << kDbSize << std::endl;
 
-  // Set up database
-  std::unordered_map<std::string, std::string> refToPrimStrs;
-  for(size_t i = 0; i < dbSize; ++i) {
-    refToPrimStrs[getEntityReference(i)] = getPrimaryString(i);
-  }
+    // Set up database
+    std::unordered_map<std::string, std::string> refToPrimStrs;
+    for(size_t i = 0; i < kDbSize; ++i) {
+      refToPrimStrs[getEntityReference(i)] = getPrimaryString(i);
+    }
 
-  // Set up a list of random refs we want to look up
-  std::vector<std::string> refs;
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  std::uniform_int_distribution<> randAssetIndexDistrib(0, dbSize - 1);
-  for(size_t i = 0; i < nIterations; ++i) {
-    const auto rnd = randAssetIndexDistrib(gen);
-    refs.emplace_back(getEntityReference(rnd));
-  }
+    // Set up a list of random refs we want to look up
+    std::vector<std::string> refs;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> randAssetIndexDistrib(0, kDbSize - 1);
+    for(size_t i = 0; i < kNIterations; ++i) {
+      const auto rnd = size_t(randAssetIndexDistrib(gen));
+      refs.emplace_back(getEntityReference(rnd));
+    }
 
-  const auto oldTime = benchmarkOldManager(refToPrimStrs, refs);
-  std::cout << "resolveEntityReference: " << oldTime << " ms" << std::endl;
+    const auto oldTime = benchmarkOldManager(refToPrimStrs, refs);
+    std::cout << "resolveEntityReference: " << oldTime << " ms" << std::endl;
 
-  const auto newTime = benchmarkNewManager(refToPrimStrs, refs);
-  std::cout << "resolve: " << newTime << " ms" << std::endl;
+    const auto newTime = benchmarkNewManager(refToPrimStrs, refs);
+    std::cout << "resolve: " << newTime << " ms" << std::endl;
+  } catch (...) { }
 }
