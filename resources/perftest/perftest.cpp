d7fe675a2232c782ff3120e67c8b8b9971d17aab
Tom Cowland <tom@foundry.com>
2022-05-27 09:09:51 +0100

    [Resources] clang-format `perftest.cpp`
    Signed-off-by: Tom Cowland <tom@foundry.com>

--- resources/perftest/perftest.cpp
+++ resources/perftest/perftest.cpp
@@ -1,28 +1,26 @@
-#include <iostream>
+#include <algorithm>
 #include <chrono>
 #include <functional>
+#include <iostream>
 #include <random>
 #include <string>
-#include <algorithm>
 #include <unordered_map>
 
 #include <openassetio/TraitsData.hpp>
 #include <openassetio/trait/TraitBase.hpp>
-using namespace openassetio; // NOLINT(google-build-using-namespace)
+using namespace openassetio;  // NOLINT(google-build-using-namespace)
 
 constexpr size_t kNIterations = 100000;
 const size_t kDbSize = 1000000;
 
-std::chrono::nanoseconds measureRuntime(const std::function<void(void)>& func)
-{
+std::chrono::nanoseconds measureRuntime(const std::function<void(void)>& func) {
   const auto start = std::chrono::high_resolution_clock::now();
   func();
   const auto stop = std::chrono::high_resolution_clock::now();
   return stop - start;
 }
 
-auto measureRuntimeMs(const std::function<void(void)>& func)
-{
+auto measureRuntimeMs(const std::function<void(void)>& func) {
   return std::chrono::duration_cast<std::chrono::milliseconds>(measureRuntime(func)).count();
 }
 
@@ -30,19 +28,16 @@ auto measureRuntimeMs(const std::function<void(void)>& func)
 /// Enlarge the path so we likely exceed the small string optimization length
 const std::string kSubDirectory = "0123456789_0123456789_0123456789/";
 
-std::string getEntityReference(size_t i)
-{
+std::string getEntityReference(size_t i) {
   return "ams://" + kSubDirectory + "myasset_" + std::to_string(i);
 }
 
-std::string getPrimaryString(size_t i)
-{
+std::string getPrimaryString(size_t i) {
   return "file://" + kSubDirectory + "myasset_" + std::to_string(i);
 }
 
 ///////////////////////////////////////////////////////////
 struct BlobTrait : trait::TraitBase<BlobTrait> {
-
   static inline const trait::TraitId kId{"blob"};
   static inline const trait::property::Key kUrl{"url"};
 
@@ -52,34 +47,31 @@ struct BlobTrait : trait::TraitBase<BlobTrait> {
     return getTraitProperty(out, kId, kUrl);
   }
 
-  void setUrl(Str url) {
-    data()->setTraitProperty(kId, kUrl, std::move(url));
-  }
+  void setUrl(Str url) { data()->setTraitProperty(kId, kUrl, std::move(url)); }
 };
 
 ///////////////////////////////////////////////////////////
-class OldManagerInterface
-{
-public:
-  explicit OldManagerInterface(const std::unordered_map<std::string, std::string>& refToPrimStrs) : refToPrimStrs_{refToPrimStrs} {}
+class OldManagerInterface {
+ public:
+  explicit OldManagerInterface(const std::unordered_map<std::string, std::string>& refToPrimStrs)
+      : refToPrimStrs_{refToPrimStrs} {}
   std::string resolveEntityReference(const std::string& ref);
 
-private:
+ private:
   const std::unordered_map<std::string, std::string>& refToPrimStrs_;
 };
 
-std::string OldManagerInterface::resolveEntityReference(const std::string& ref)
-{
+std::string OldManagerInterface::resolveEntityReference(const std::string& ref) {
   return refToPrimStrs_.at(ref);
 }
 
 ///////////////////////////////////////////////////////////
-auto benchmarkOldManager(const std::unordered_map<std::string, std::string>& refToPrimStrs, const std::vector<std::string>& refs)
-{
+auto benchmarkOldManager(const std::unordered_map<std::string, std::string>& refToPrimStrs,
+                         const std::vector<std::string>& refs) {
   OldManagerInterface oldMgr(refToPrimStrs);
 
   const auto doLookups = [&oldMgr, &refs]() {
-    for(size_t i = 0; i < kNIterations; ++i) {
+    for (size_t i = 0; i < kNIterations; ++i) {
       const auto& ref = refs[i];
       [[maybe_unused]] const auto& _ = oldMgr.resolveEntityReference(ref);
     }
@@ -89,26 +81,27 @@ auto benchmarkOldManager(const std::unordered_map<std::string, std::string>& ref
 }
 
 ///////////////////////////////////////////////////////////
-class NewManagerInterface
-{
-public:
-  explicit NewManagerInterface(const std::unordered_map<std::string, std::string>& refToPrimStrs) : refToPrimStrs_{refToPrimStrs} {}
-  std::shared_ptr<TraitsData> resolve(const std::string& ref, const TraitsData::TraitSet& traitSet);
+class NewManagerInterface {
+ public:
+  explicit NewManagerInterface(const std::unordered_map<std::string, std::string>& refToPrimStrs)
+      : refToPrimStrs_{refToPrimStrs} {}
+  std::shared_ptr<TraitsData> resolve(const std::string& ref,
+                                      const TraitsData::TraitSet& traitSet);
 
-private:
+ private:
   const std::unordered_map<std::string, std::string>& refToPrimStrs_;
 };
 
-std::shared_ptr<TraitsData> NewManagerInterface::resolve(const std::string& ref, const TraitsData::TraitSet& traitSet)
-{
+std::shared_ptr<TraitsData> NewManagerInterface::resolve(const std::string& ref,
+                                                         const TraitsData::TraitSet& traitSet) {
   TraitsData::TraitSet populatedTraits;
   bool getPath = false;
-  if(std::find(traitSet.cbegin(), traitSet.cend(), BlobTrait::kId) != traitSet.cend()) {
+  if (std::find(traitSet.cbegin(), traitSet.cend(), BlobTrait::kId) != traitSet.cend()) {
     populatedTraits.insert(BlobTrait::kId);
     getPath = true;
   }
   auto result = std::make_shared<TraitsData>(populatedTraits);
-  if(getPath) {
+  if (getPath) {
     BlobTrait btrait(result);
     btrait.setUrl(refToPrimStrs_.at(ref));
   }
@@ -116,12 +109,12 @@ std::shared_ptr<TraitsData> NewManagerInterface::resolve(const std::string& ref,
 }
 
 ///////////////////////////////////////////////////////////
-auto benchmarkNewManager(const std::unordered_map<std::string, std::string>& refToPrimStrs, const std::vector<std::string>& refs)
-{
+auto benchmarkNewManager(const std::unordered_map<std::string, std::string>& refToPrimStrs,
+                         const std::vector<std::string>& refs) {
   NewManagerInterface newMgr(refToPrimStrs);
 
   const auto doLookups = [&newMgr, &refs]() {
-    for(size_t i = 0; i < kNIterations; ++i) {
+    for (size_t i = 0; i < kNIterations; ++i) {
       const auto& ref = refs[i];
       const auto spec = newMgr.resolve(ref, {BlobTrait::kId});
       BlobTrait btrait(spec);
@@ -134,19 +127,18 @@ auto benchmarkNewManager(const std::unordered_map<std::string, std::string>& ref
 }
 
 ///////////////////////////////////////////////////////////
-int main()
-{
-  try { // bugprone-exception-escape
+int main() {
+  try {  // bugprone-exception-escape
     std::cout << "CAUTION: on Windows do not run this in a Visual Studio command prompt "
-      << "('x64 Native Tools Command Prompt for VS') or the measurements may fluctuate, "
-      << "use a basic CMD shell" << std::endl;
+              << "('x64 Native Tools Command Prompt for VS') or the measurements may fluctuate, "
+              << "use a basic CMD shell" << std::endl;
 
     std::cout << "       Iterations: " << kNIterations << std::endl;
     std::cout << "AMS database size: " << kDbSize << std::endl;
 
     // Set up database
     std::unordered_map<std::string, std::string> refToPrimStrs;
-    for(size_t i = 0; i < kDbSize; ++i) {
+    for (size_t i = 0; i < kDbSize; ++i) {
       refToPrimStrs[getEntityReference(i)] = getPrimaryString(i);
     }
 
@@ -155,7 +147,7 @@ int main()
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> randAssetIndexDistrib(0, kDbSize - 1);
-    for(size_t i = 0; i < kNIterations; ++i) {
+    for (size_t i = 0; i < kNIterations; ++i) {
       const auto rnd = size_t(randAssetIndexDistrib(gen));
       refs.emplace_back(getEntityReference(rnd));
     }
@@ -165,5 +157,6 @@ int main()
 
     const auto newTime = benchmarkNewManager(refToPrimStrs, refs);
     std::cout << "resolve: " << newTime << " ms" << std::endl;
-  } catch (...) { }
+  } catch (...) {
+  }
 }
