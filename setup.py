67ef548062503e1e18b8bd8412ee337c459a16cf
David Feltell <david.feltell@foundry.com>
2022-08-19 17:19:55 +0100

    [Release] Bump version to 1.0.0-alpha.3
    Signed-off-by: David Feltell <david.feltell@foundry.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='openassetio',
-    version="1.0.0-alpha.2",
+    version="1.0.0-alpha.3",
     package_dir={'': 'python'},
     packages=find_packages(where='python'),
     python_requires='>=3.7',
