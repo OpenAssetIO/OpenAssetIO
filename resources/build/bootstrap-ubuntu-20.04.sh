16bdcbe992400333d0f693134a946eb23341458f
David Feltell <david.feltell@foundry.com>
2022-09-07 11:49:56 +0100

    Revert "Parallelise bootstrap"
    This reverts commit 754a000c9aee68644f57e58d77271eee94215706.

--- resources/build/bootstrap-ubuntu-20.04.sh
+++ resources/build/bootstrap-ubuntu-20.04.sh
@@ -3,29 +3,24 @@
 # Vagrant VM configuration.
 
 set -xeo pipefail
-# Install additional build tools.
-sudo pip3 install -r "$WORKSPACE/resources/build/requirements.txt" &
-
+sudo apt-get update
 # Install gcc, linters, build tools used by conan and Python 3.
+sudo apt-get install -y build-essential pkgconf clang-format-12 clang-tidy-12 python3-pip
 # Install system packages required for the Python 3.9 conan package.
 # These would be installed as part of the conan package install, but
 # we're caching the conan directory via the `actions/cache` Github
 # action, so a fresh Github VM is left without these system packages.
-sudo apt-get update && \
-sudo apt-get install -y build-essential pkgconf clang-format-12 clang-tidy-12 python3-pip \
- libfontenc-dev libice-dev libsm-dev libx11-dev \
+sudo apt-get install -y --no-install-recommends libfontenc-dev libice-dev libsm-dev libx11-dev \
  libx11-xcb-dev libxau-dev libxaw7-dev libxcb-dri3-dev libxcb-icccm4-dev libxcb-image0-dev \
  libxcb-keysyms1-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev \
  libxcb-shape0-dev libxcb-sync-dev libxcb-util-dev libxcb-xfixes0-dev libxcb-xinerama0-dev \
  libxcb-xkb-dev libxcomposite-dev libxcursor-dev libxdamage-dev libxdmcp-dev libxext-dev \
  libxfixes-dev libxi-dev libxinerama-dev libxkbfile-dev libxmu-dev libxmuu-dev libxpm-dev \
  libxrandr-dev libxrender-dev libxres-dev libxss-dev libxt-dev libxtst-dev libxv-dev libxvmc-dev \
- libxxf86vm-dev uuid-dev xkb-data xtrans-dev &
+ libxxf86vm-dev uuid-dev xkb-data xtrans-dev
 
-
-# Wait for child ('&') processes.
-wait
-
+# Install additional build tools.
+sudo pip3 install -r "$WORKSPACE/resources/build/requirements.txt"
 # Use explicit predictable conan root path, to be used for both packages
 # and conan CMake toolchain config.
 export CONAN_USER_HOME="$HOME/conan"
@@ -36,7 +31,6 @@ conan profile new default --detect --force
 # necessary as this is the default for conan, but we can't be certain
 # it'll remain the default in future.
 conan profile update settings.compiler.libcxx=libstdc++ default
-
 # Install openassetio third-party dependencies from public Conan Center
 # package repo.
 # TODO(DF): conan<1.51 (not yet released) has a bug that means we have
