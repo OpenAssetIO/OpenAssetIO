1fa85c733f62548fdc259d76ab9392f2c84f0aee
David Feltell <david.feltell@foundry.com>
2022-10-18 14:11:26 +0100

    [Build] Migrate to `conanfile.py` from `.txt`
    Part of #630. In order to better support Windows builds using
    ConanCenter's `cpython` package, we must enable the `shared` option.
    However, this breaks Linux builds (due to executable vs. library RPATH
    issues). So we need to enable this option only if using Windows. This
    precludes using the simpler `conanfile.txt`, where such conditionality
    is not possible.
    
    So migrate to a basic `conanfile.py` that has the same effect as the
    previous `conanfile.txt`, except with the aforementioned conditional
    `cpython` option.
    
    This also paves the way for future work:
    - Automating Python wheel building, where we want to build against the
      external environment's Python, rather than a fixed conan `cpython`
      package.
    - Packaging OpenAssetIO up as a Conan package, for redistribution.
    
    Signed-off-by: David Feltell <david.feltell@foundry.com>

--- resources/build/bootstrap-windows-2019.bat
+++ resources/build/bootstrap-windows-2019.bat
@@ -8,5 +8,4 @@ conan profile new default --detect --force
 conan profile show default
 :: Install openassetio third-party dependencies from public Conan Center
 :: package repo.
-:: Doesn't build properly on Windows without cpython:shared=True. See: https://github.com/conan-io/conan-center-index/issues/9333
-conan install -o cpython:shared=True --install-folder "%CONAN_USER_HOME%" --build=missing "%WORKSPACE%\resources\build"
+conan install --install-folder "%CONAN_USER_HOME%" --build=missing "%WORKSPACE%\resources\build"
