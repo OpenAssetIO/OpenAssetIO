be498b5727fe8885a424fcdfae22d7bfa3493de8
David Feltell <david.feltell@foundry.com>
2022-11-24 11:23:28 +0000

    fixup! [Tests] Split out "bridge" tests from "internal"
    Remove defunct TODO
    
    Signed-off-by: David Feltell <david.feltell@foundry.com>

--- src/openassetio-python/tests/bridge/main.cpp
+++ src/openassetio-python/tests/bridge/main.cpp
@@ -8,8 +8,5 @@ namespace py = pybind11;
 
 int main(int argc, char* argv[]) {
   py::scoped_interpreter guard{};
-  // TODO(DF): blocks because GIL is acquired here and then attempted again
-  // in _openassetio, but _openassetio thinks it's in a different thread
-  // most probably symbol visiblity problems.
   return Catch::Session().run(argc, argv);
 }
