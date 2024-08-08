701c9a45ee4506e3ea002a9963f1f171198dd742
David Feltell <david.feltell@foundry.com>
2024-08-08 13:32:39 +0100

    [CI] Bump runners to approximate VFX CY24
    Closes #812. Closes #1354. Part of #1389.
    
    Bump/pin to Ubuntu 22.04 and Windows 2022 runners, which most closely
    resemble VFX Reference Platform CY24. Similarly, bump the default Python
    version used on CI to 3.11 to better match CY24.
    
    For MacOS, there isn't a specific VFX Reference Platform requirement, so
    leave as-is for now. That is, use the oldest version available on the
    assumption that forward compatibility is more likely than backward
    compatibility. MacOS will be futher addressed in #718.
    
    Remove the boostrap script for macos-10.15 - it is unused.
    
    Signed-off-by: David Feltell <david.feltell@foundry.com>

--- resources/build/bootstrap-ubuntu-22.04.sh
+++ resources/build/bootstrap-ubuntu-22.04.sh
@@ -13,10 +13,8 @@ export CONAN_USER_HOME="$HOME/conan"
 # Create default conan profile so we can configure it before install.
 # Use --force so that if it already exists we don't error out.
 conan profile new default --detect --force
-# Use new/non-deprecated C++11 ABI to match GitHub runner's default.
-# This does not match VFX Reference Platform CY22, which is currently
-# our default target, but official CY22 support (including CI testing)
-# is due to be removed soon.
+# Use new/non-deprecated C++11 ABI to match VFX Reference Platform CY24
+# (and GitHub runner's default).
 conan profile update settings.compiler.libcxx=libstdc++11 default
 # If we need to pin a package to a specific Conan recipe revision, then
 # we need to explicitly opt-in to this functionality.
