83c487c905ef7aa112752db01e515a658e1b62d0
David Feltell <david.feltell@foundry.com>
2025-03-19 12:00:54 +0000

    [Python] Simplify Python binding for CppPluginSystemPlugin
    Part of #1445.
    
    The `CppPluginSystem` is exposed to Python, which was never really
    necessary, since it's really an implementation detail of
    `CppPluginSystemManagerImplementationFactory`. Having Python bindings
    does help with tests (since we can test it through Python). But it means
    we need to also bind `CppPluginSystemPlugin` (since instances of it
    are returned by `CppPluginSystem`).
    
    The `CppPluginSystemPlugin` class is only used (inherited from) by C++
    plugins, and Python will never interact directly with these. It makes no
    sense to construct (or inherit from) a `CppPluginSystemPlugin` in
    Python.
    
    So keep the binding of `CppPluginSystemPlugin`, but simplify to remove
    the ability to construct or inherit from it. This simplifies tests and
    clarifies its usage pattern.
    
    Don't bother adding to RELEASE_NOTES since this was never supported
    functionality.
    
    Signed-off-by: David Feltell <david.feltell@foundry.com>

--- src/openassetio-python/tests/cmodule/gil/test_cpppluginsystem_gil.py
+++ src/openassetio-python/tests/cmodule/gil/test_cpppluginsystem_gil.py
@@ -23,51 +23,13 @@ import sysconfig
 # pylint: disable=redefined-outer-name,protected-access
 # pylint: disable=invalid-name,c-extension-no-member
 # pylint: disable=missing-class-docstring,missing-function-docstring
-from unittest import mock
 
 import openassetio
 import pytest
 
 # pylint: disable=no-name-in-module
-from openassetio import _openassetio
 from openassetio.managerApi import ManagerInterface
-from openassetio.pluginSystem import (
-    CppPluginSystem,
-    CppPluginSystemPlugin,
-    CppPluginSystemManagerImplementationFactory,
-)
-
-
-class Test_CppPluginSystemPlugin_gil:
-    """
-    Check all methods release the GIL during C++ function body
-    execution.
-
-    See docstring for similar test under `gil/Test_ManagerInterface.py`
-    for details on how these tests are structured.
-    """
-
-    def test_all_methods_covered(self, find_unimplemented_test_cases):
-        """
-        Ensure this test class covers all methods.
-        """
-        unimplemented = find_unimplemented_test_cases(CppPluginSystemPlugin, self)
-
-        if unimplemented:
-            print("\nSome test cases not implemented. Method templates can be found below:\n")
-            for method in unimplemented:
-                print(
-                    f"""
-    def test_{method}(self, a_threaded_cpp_plugin):
-        a_threaded_cpp_plugin.{method}()
-"""
-                )
-
-        assert unimplemented == []
-
-    def test_identifier(self, mock_cpp_plugin, a_threaded_cpp_plugin):
-        mock_cpp_plugin.mock.identifier.return_value = "something"
-        a_threaded_cpp_plugin.identifier()
+from openassetio.pluginSystem import CppPluginSystem, CppPluginSystemManagerImplementationFactory
 
 
 class Test_CppPluginSystem_gil:
@@ -194,30 +156,6 @@ def a_cpp_plugin_impl_factory(the_cpp_gil_check_plugin_path, a_threaded_logger_i
     return CppPluginSystemManagerImplementationFactory(
         the_cpp_gil_check_plugin_path, a_threaded_logger_interface
     )
-
-
-@pytest.fixture
-def a_threaded_cpp_plugin(mock_cpp_plugin):
-    return _openassetio._testutils.gil.wrapInThreadedCppPluginSystemPlugin(mock_cpp_plugin)
-
-
-@pytest.fixture
-def mock_cpp_plugin():
-    return MockCppPluginSystemPlugin()
-
-
-class MockCppPluginSystemPlugin(CppPluginSystemPlugin):
-    """
-    `CppPluginSystemPlugin` implementation that delegates all
-     calls to a public `Mock` instance.
-    """
-
-    def __init__(self):
-        super().__init__()
-        self.mock = mock.create_autospec(CppPluginSystemPlugin, spec_set=True, instance=True)
-
-    def identifier(self):
-        return self.mock.identifier()
 
 
 @pytest.fixture(scope="session")
